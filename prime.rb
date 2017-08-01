9792726749f6364076eb6a7bd6e51565dd21a82d
Natalie Perpepaj <perpepajn25@gmail.com>
2017-08-01 15:52:01 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+    if num <= 1
+        return false
+    end
+    (2...num).each do |divisor|
+        if num%divisor == 0
+            return false
+        end
+    end
+    return true
+end
